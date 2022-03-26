5355652190ff05d6805a1576cd9e6c593a04270f
Yerbol Syzdyk <erbolsyzdyq@gmail.com>
2022-03-26 12:09:34 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { CommentBlock } from './components/CommentBlock'
+
+function App() {
+  return (
+    <div className="App">
+        Comments Block
+        <CommentBlock />
+        <CommentBlock />
+        <CommentBlock />
+    </div>
+  );
+}
+
+export default App;
