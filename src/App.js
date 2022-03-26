7b8a2178ae8b75d1d6863f1d7cd1263f47eab20f
Yerbol Syzdyk <erbolsyzdyq@gmail.com>
2022-03-26 13:10:13 +0600

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import './App.css';
 import { CommentBlock } from './components/CommentBlock'
-
-const comments = [
-    {
-        text: 'Hot Stuff!',
-        created: '11:00 26.03.2022',
-        authorName: 'Vasya Pupkin',
-        authorImage: 'https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4'
-    },
-    {
-        text: 'Hot Stuff 2!',
-        created: '11:00 26.03.2022',
-        authorName: 'Almas',
-        authorImage: 'https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4'
-    },
-    {
-        text: 'Hot Stuff 3!',
-        created: '11:00 26.03.2022',
-        authorName: 'Vasya Pupkin',
-        authorImage: 'https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4'
-    },
-]
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-        Comments Block
-        {comments.map((comment) =>
-            <CommentBlock comment={comment} />
-        )}
-    </div>
-  );
+    const [comments, setComments] = useState([])
+
+
+    fetch('https://kdwed-f1dd2-default-rtdb.europe-west1.firebasedatabase.app/comments.json')
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+            setComments(data);
+            return data
+        })
+
+    return (
+        <div className="App">
+            Comments Block
+            {comments.map((comment) =>
+                <CommentBlock comment={comment} />
+            )}
+        </div>
+    );
 }
 
 export default App;
--- src/components/CommentBlock.js
+++ src/components/CommentBlock.js
@@ -1,13 +1,20 @@
-export function CommentBlock({ comment }) {
+function CommentTop({ comment }) {
+    return (
+        <div className="top">
+            <img className="avatar" src={comment.author.avatarUrl} alt="" />
+            <span className="name">{comment.author.name}</span>
+            <span className="date">{comment.created}</span>
+        </div>
+    )
+}
+
+export function CommentBlock({ comment, suffix }) {
     return (
         <div className="comment_block">
-            <div className="top">
-                <img className="avatar" src={comment.authorImage} alt="" />
-                <span className="name">{comment.authorName}</span>
-                <span className="date">{comment.created}</span>
-            </div>
+            <CommentTop comment={comment} />
             <div className="text">
                 {comment.text}
+                {suffix}
             </div>
         </div>
     )
