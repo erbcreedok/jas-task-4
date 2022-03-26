4751d37c777617847a39bb2368dfff63f94fb959
Yerbol Syzdyk <erbolsyzdyq@gmail.com>
2022-03-26 12:17:17 +0600

    render array

--- src/components/CommentBlock.js
+++ src/components/CommentBlock.js
@@ -1,13 +1,13 @@
-export function CommentBlock() {
+export function CommentBlock({ comment }) {
     return (
         <div className="comment_block">
             <div className="top">
-                <img className="avatar" src="https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4" alt="" />
-                <span className="name">Vasya Pupkin</span>
-                <span className="date">11:00 26.03.2022</span>
+                <img className="avatar" src={comment.authorImage} alt="" />
+                <span className="name">{comment.authorName}</span>
+                <span className="date">{comment.created}</span>
             </div>
             <div className="text">
-                Hot Stuff!
+                {comment.text}
             </div>
         </div>
     )
